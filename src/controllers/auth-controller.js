48a170fa31ff57c796769e2643ef537bc46cf079
AlvesBenvindo <alvesndungula2020@gmail.com>
2025-11-08 22:30:12 +0100

    feat:mvc implementado

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,31 @@
+import path from "path";
+
+import { AuthService } from "../services/auth-service.js";
+import { UserService } from "../services/user-service.js";
+
+export const formLogin = async (req, res) => {
+    // const users = await UserService.listar();
+    res.sendFile(path.join(process.cwd(), "src/views/auth/login.html"));
+};
+
+export const login = async (req, res) => {
+    try {
+        const user = await UserService.buscarPorId(req.params.id);
+        res.render("users/show", { user });
+    } catch (err) {
+        res.status(404).send(err.message);
+    }
+};
+
+export const register = async (req, res) => {
+    try {
+        const user = await UserService.buscarPorId(req.params.id);
+        res.render("users/show", { user });
+    } catch (err) {
+        res.status(404).send(err.message);
+    }
+};
+
+export const logout = async (req, res) => {
+    res.render("users/create");
+};
