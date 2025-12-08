f6db9374b403498b8a8d782274e1884f5bbc0f62
AlvesBenvindo <alvesndungula2020@gmail.com>
2025-12-08 16:47:53 +0100

    feat: criando tabelas na base de dados a partir da API.__

--- src/config/app/express/server.js
+++ src/config/app/express/server.js
@@ -9,6 +9,8 @@ import authRoutes from "./routes/auth-routes.js";
 import systemRoutes from "./routes/system-routes.js";
 import userRoutes from "./routes/user-routes.js";
 
+import { criarTodasTabelas } from "../../database/index.js";
+
 dotenv.config();
 
 // const __filename = fileURLToPath(import.meta.url);
@@ -41,6 +43,8 @@ const port = process.env.PORT || 3000;
 
 export const startServer = 
   () => 
-    app.listen(port, () => {
+    app.listen(port, async () => {
+      console.log(`Criando tabelas na base de dados, se não existirem...`);
+      await criarTodasTabelas();
       console.log(`Server On Fire on port: http://localhost:${port}`);
     });
--- src/server.js
+++ src/server.js
@@ -1,18 +1,7 @@
-import app from "./config/app/express/server.js";
+import { startServer } from "./config/app/express/server.js";
 import dotenv from "dotenv";
-import { criarTodasTabelas } from "./config/database/index.js";
 
 
 dotenv.config();
-
-const port = process.env.PORT || 3000;
-
-const startServer = 
-  () => 
-    app.listen(port, async () => {
-      console.log(`Criando tabelas na base de dados, se não existirem...`);
-      await criarTodasTabelas();
-      console.log(`Server On Fire on port: http://localhost:${port}`);
-    });
 
 startServer();
